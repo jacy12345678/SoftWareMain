2fee9911d160fbd6761195c4f247bceab45734f5
jacy12345678 <94233336+jacy12345678@users.noreply.github.com>
2021-12-26 11:38:08 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,22 +1,15 @@
-function home(){
-    navigation.classList.remove("active");
-    main.classList.remove("active");
-    table1.classList.remove("active");
-    window.location="index.html";
-
-}
-function showclass(){
-    navigation.classList.remove("active");
-    main.classList.remove("active");
-    table1.classList.remove("active");
-    window.location="class.html";
-}
+var mainaccount;
+var mainpw;
+var classArraay=[];	
+var studentID=[];
+var studentName=[];
+var studentState=[];
+var teachername;
+var indexclass;
 function signin(){
     navigation.classList.remove("active");
     main.classList.remove("active");
     table1.classList.remove("active");
-    window.location="signIn.html";
-    
 }
 let toggle=document.querySelector(".toggle");
 let navigation=document.querySelector('.nevigation');
@@ -33,3 +26,20 @@ selectElement.addEventListener('change', (event) => {
   const result = document.querySelector('.lable');
   result.textContent = `${event.target.value}`;
 });
+function addclass(){
+    for(var i=2;i<classArraay.length;i++){
+        document.myForm.member.options[i-2]=new Option(classArraay[i]);	// 設定新選項
+        document.myForm.member.length=classArraay.length-2;
+    }
+    
+    
+}
+function classchange(index){
+  indexclass=classArraay[index+2];
+  console.log(indexclass);
+}
+function setname(){
+  let tname=document.getElementById("tname");
+  tname.textContent=teachername;
+}
+
